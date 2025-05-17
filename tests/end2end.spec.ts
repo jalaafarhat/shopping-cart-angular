0873d8c57f1f67d1c06301ec1bab7283ea7b8e04
jalaa farhat <jalaa.c.m@gmail.com>
2025-05-17 12:55:33 +0300

    end to end testing using playwright for signup, login, home, products, cart.

--- tests/end2end.spec.ts
+++ tests/end2end.spec.ts
@@ -1,9 +1,50 @@
 import { test, expect } from "@playwright/test";
 
-test("ensure unauthenticated users are redirected to the login page", async ({
-  page,
-}) => {
-  await page.goto("/");
-
+test("ensure redirect to the login page at the start", async ({ page }) => {
+  await page.goto("http://localhost:4200/");
   await expect(page).toHaveURL("/login");
+});
+
+test("End to end test", async ({ page }) => {
+  // Signup
+  await page.goto("http://localhost:4200/signup");
+
+  await page.fill('input[name="email"]', "user@example.com");
+  await page.fill('input[name="pass"]', "A12345");
+  await page.fill('input[name="cpass"]', "A12345");
+  await page.click('button[name="signup"]');
+
+  // Wait for redirect to login
+  await page.waitForURL("http://localhost:4200/login", { timeout: 3000 });
+
+  // Login
+  await page.fill('input[name="email"]', "user@example.com");
+  await page.fill('input[name="pass"]', "A12345");
+  await page.click('button[name="login"]');
+
+  // Redirect to homepage
+  await expect(page).toHaveURL("http://localhost:4200/home");
+
+  // Go to Products from home header
+  await page.click('button[name="products"]');
+  await page.waitForURL("http://localhost:4200/products");
+
+  // Choose size and quantity for 1st product
+  const firstProduct = page.locator(".product-card").nth(0);
+  await firstProduct.locator("select").selectOption({ label: "40" });
+  await firstProduct.locator('input[name="quantity"]').fill("1");
+  await firstProduct.locator('button[name="add"]').click();
+
+  // Choose size and quantity for 7st product
+  const secondProduct = page.locator(".product-card").nth(6);
+  await secondProduct.locator("select").selectOption({ label: "M" });
+  await secondProduct.locator('input[name="quantity"]').fill("3");
+  await secondProduct.locator('button[name="add"]').click();
+
+  // Navigate to cart
+  await page.click('button[name="cart"]');
+  await page.waitForURL("http://localhost:4200/cart");
+
+  // Verify cart has items
+  await expect(page.locator(".cart-card")).toHaveCount(2);
 });
